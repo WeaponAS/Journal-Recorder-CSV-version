36c090633094749701433cd20a26ea31f844d236
WeaponAS <97493358+WeaponAS@users.noreply.github.com>
2023-02-21 07:38:30 +0530

    Solved some Errors

--- DayTimeMonth.py
+++ DayTimeMonth.py
@@ -39,44 +39,31 @@ def CDate():
     t=print(today)
     return t
 
-def Month_Name_Today_oprational(MNopration,MNScale):
-    raise ConnectionAbortedError("This Module is still not finish building and have inpurities")
-    MN=today.month
-    ml=np.array[-1, -13, -25]
-    while True:
-        if MNopration=="+":
-            newNM=MN-int(MNScale)
-            break
-        elif MNopration=="-":
-            if MN-MNScale<1:
-                temp=MN-MNScale
-                if temp in ml:
-                    newNM="December"
-                elif temp in ml-1:
-                    newNM="November"
-                elif temp in ml-2:
-                    newNM="October"
-                elif temp in ml-3:
-                    newNM="September"
-                elif temp in ml-4:
-                    newNM="August"
-                elif temp in ml-5:
-                    newNM="July"
-                elif temp in ml-6:
-                    newNM="June"
-                elif temp in ml-7:
-                    newNM="May"
-                elif temp in ml-8:
-                    newNM="April"
-                elif temp in ml-9:
-                    newNM="March"
-                elif temp in ml-10:
-                    newNM="February"
-                elif temp in ml-11:
-                    newNM="January"
-                else:
-                    print("Only Support for last 3 years")
-            break
-        else:
-            print("Not a valid input")
-    return newNM
+def Month_Name_oprational(Month):
+    if Month==1:
+        MN="January"
+    elif Month==2:
+        MN="February"
+    elif Month==3:
+        MN="March"
+    elif Month==4:
+        MN="April"
+    elif Month==5:
+        MN="May"
+    elif Month==6:
+        MN='June'
+    elif Month==7:
+        MN="July"
+    elif Month==8:
+        MN="August"
+    elif Month==9:
+        MN="Suptember"
+    elif Month==10:
+        MN="October"
+    elif Month ==11:
+        MN="November"
+    elif Month==12:
+        MN="December"
+    else:
+        print("I have a dought are you really from this planet")
+    return MN
--- Main.py
+++ Main.py
@@ -27,10 +27,18 @@ while True:
     # Changed inflow to income for clarity
     print("\nBalance : ", df.inflow.sum() - df.outflow.sum())
     print("1) Record Journal Entries")
-    print("2) Show Statistics (by default this month) ")
-    print("3) Analysis")
+    print("2) Show Statistics (Details)")
+    print("3) Analysis (Data Visualising)")
     print("0) Quit")
-    MainOperation = int(input("\nEnter one of the above mentioned to perform Activity (Integer only): "))
+    while True:
+        try:
+            MainOperation = int(input("\nEnter one of the above mentioned to perform Activity (Integer only): "))
+        except ValueError:
+            print("Not a Valid Input type")
+            print("Type only integers that is shown in main menu")
+        finally:
+            break
+
     if MainOperation == 1:
         print("="*30)
         print("\n\nRecording Journal\n\n")
@@ -70,8 +78,10 @@ while True:
                 tempdf = df[df['Particular']==item]
                 total = tempdf['inflow'].sum() - tempdf['outflow'].sum()
                 print(item + ":", total)
-                AnyKey = input("Press Enter to Return to main menu : ")
+                AnyKey = input("Enter any key to Return to main menu : ")
                 if AnyKey=="":
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                else:
                     os.system('cls' if os.name == 'nt' else 'clear')
         elif StatOption.upper() == 'Y':
             print('Current Total : ', df.inflow.sum() - df.outflow.sum())
@@ -84,36 +94,68 @@ while True:
                 if AnyKey=="":
                     os.system('cls' if os.name == 'nt' else 'clear')
     elif MainOperation==3:
-        AnalysisOption=input("Do you Want to Compair Analysis (Y/N): ")
-        if AnalysisOption.upper()=='N':
-            #display inflow and outflow for current month for each day
-            df.set_index("Date",inplace=True)
-            df[["inflow","outflow"]].plot(kind='bar',color=['Green','Red'])
-            plt.xlabel("Date")
-            plt.ylabel("Amount")
-            plt.title(file)
-            plt.subplots_adjust(bottom=0.25)
-            plt.show()       
-        elif AnalysisOption.upper()=="Y":
-            plt.figure()
-            df.set_index("Date",inplace=True)
-            df[["inflow","outflow"]].plot(kind='bar',color=['Green','Red'])
-            plt.xlabel("Date")
-            plt.ylabel("Amount")
-            plt.title(file)
-            plt.subplots_adjust(bottom=0.25)
-            plt.show()
-            plt.figure()
-            
-            
-            dft="{}-{}.csv".format(MN,Year)
-            dft.set_index("Date",inplace=True)
-            dft[["inflow","outflow"]].plot(kind='bar',color=['Green','Red'])
-            plt.xlabel("Date")
-            plt.ylabel("Amount")
-            plt.title(file)
-            plt.subplots_adjust(bottom=0.25)
-            plt.show()
+        while True:
+            AnalysisOption=input("Do you Want to Compair Analysis (Y/N): ")
+            if AnalysisOption.upper()=='N':
+                #display inflow and outflow for current month for each day
+                df.set_index("Date",inplace=True)
+                df[["inflow","outflow"]].plot(kind='bar',color=['Green','Red'])
+                plt.xlabel("Date")
+                plt.ylabel("Amount")
+                plt.title(file)
+                plt.subplots_adjust(bottom=0.25)
+                plt.show()
+                AnyKey=input("Press Enter any key to return to main menu : ")
+                if AnyKey=="":
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    break
+                else:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    break
+            elif AnalysisOption.upper()=="Y":
+
+                # Create a figure with two subplots
+                fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+                # Plot the first bar chart in the first subplot
+                df.set_index("Date", inplace=True)
+                df[["inflow", "outflow"]].plot(kind='bar', color=['Green', 'Red'], ax=ax1)
+                ax1.set_xlabel("Date")
+                ax1.set_ylabel("Amount")
+                ax1.set_title(file)
+                ax1.tick_params(axis='x', labelrotation=45)
+
+                # Plot the second bar chart in the second subplot
+                Year = int(input("Which Years Data You Want to Visualise (YYYY): "))
+                Month = int(input("Enter Which month of above mentioned Year (MM): "))
+                MN = DayTimeMonth.Month_Name_oprational(Month)
+                dft = "{}-{}.csv".format(MN, Year)
+                try:
+                    df2 = pd.read_csv(dft)
+                except FileNotFoundError:
+                    print("No record Found for {}")
+                    exit()
+                except pd.errors.EmptyDataError:
+                    print("Oprations Cannot be performe on empty CSV file")
+                    exit()
+                df2.set_index("Date", inplace=True)
+                df2[["inflow", "outflow"]].plot(kind='bar', color=['Green', 'Red'], ax=ax2)
+                ax2.set_xlabel("Date")
+                ax2.set_ylabel("Amount")
+                ax2.set_title(dft)
+                ax2.tick_params(axis='x', labelrotation=45)
+
+                # Adjust the layout of the subplots and display the figure
+                plt.subplots_adjust(bottom=0.25)
+                plt.show()
+                AnyKey = input("Enter any key to return to main menu : ")
+                if AnyKey=="":
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    break
+                else:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    break
+
     elif MainOperation==4:
         print("Created a CSV txt file")
         df.to_csv("read.txt",index=False)
